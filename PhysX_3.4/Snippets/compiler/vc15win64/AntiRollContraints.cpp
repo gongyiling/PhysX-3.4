38c46eb4fcebd6ecf677435b19ead1274dd494be
fractal <245044518@qq.com>
2021-07-03 18:08:15 +0800

    xxx.

--- PhysX_3.4/Snippets/compiler/vc15win64/AntiRollContraints.cpp
+++
@@ -1,6 +0,0 @@
-#include "AntiRollContraints.h"
-
-PxU32 AntiRollContraints::antiRollConstraintSolverPrep(Px1DConstraint* constraints, PxVec3& body0WorldOffset, PxU32 maxConstraints, PxConstraintInvMassScale&, const void* constantBlock, const PxTransform& bodyAToWorld, const PxTransform& bodyBToWorld)
-{
-    return PxU32();
-}
